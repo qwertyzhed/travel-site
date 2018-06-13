1ba6b7a42f67bad2e009080d217254667ef59951
Alex <alex@aslee.co.uk>
2018-06-13 18:10:28 +0100

    Lesson 20

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss')
 autop = require('autoprefixer'),
 pcvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssimport, pcvars, nested, autop]))
+  .pipe(postcss([cssimport, mixins, pcvars, nested, autop]))
   .on('error', function(errorInfo) {
     console.log(errorInfo.toString);
     this.emit('end');

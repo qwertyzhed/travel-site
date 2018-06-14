04522a78f019cb7f134068a272d952a9ff002bcf
Alex <alex@aslee.co.uk>
2018-06-14 13:48:38 +0100

    Finished testimonials

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss')
-autop = require('autoprefixer'),
-pcvars = require('postcss-simple-vars'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import'),
+cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssimport, mixins, pcvars, nested, autop]))
-  .on('error', function(errorInfo) {
-    console.log(errorInfo.toString);
-    this.emit('end');
-  })
-  .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
